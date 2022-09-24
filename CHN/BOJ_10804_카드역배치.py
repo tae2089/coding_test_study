cffb250ac2ce2da4bf59b7365e16247474fb51bc
haneul jo <77534419+jo999999@users.noreply.github.com>
2022-09-24 15:48:28 +0900

    Update BOJ_10804_카드역배치.py

--- CHN/BOJ_10804_카드역배치.py
+++ CHN/BOJ_10804_카드역배치.py
@@ -1,8 +1,8 @@
 a = list(range(21))
 for _ in range(10) :
-  s, e = map(int, input().split())
-  for i in range((e-s+1)//2):
-    a[s+i], a[e-i] = a[e-i], a[s+i]
+  start, fin = map(int, input().split())
+  for i in range((fin-start+1)//2):
+    a[start+i], a[fin-i] = a[fin-i], a[start+i]
 
 a.pop(0)  
 for x in a:
