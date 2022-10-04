8c8d3678a49bad0c8e6851cd2f9fa0527c45f5a5
tae2089 <tae2089002@gmail.com>
2022-10-04 21:03:02 +0900

    [LEECODE-112] Path Sum

--- 태빈/go/leetcode/lee_112.go
+++ 태빈/go/leetcode/lee_112.go
@@ -4,8 +4,8 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
 	if root == nil {
 		return false
 	}
-	if targetSum == 0 && root.Left == nil && root.Right == nil {
+	if targetSum-root.Val == 0 && root.Left == nil && root.Right == nil {
 		return true
 	}
-	return hasPathSum(root.Left, targetSum-root.Val) && hasPathSum(root.Right, targetSum-root.Val)
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
 }
